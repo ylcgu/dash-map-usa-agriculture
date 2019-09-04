9ca7f5bf50301ca24f852a5fab346840537ea391
ylcgu <54045235+ylcgu@users.noreply.github.com>
2019-09-04 19:03:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 
        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
 
 mycolumn='veggies fresh'
-myheading1 = "Wow! That's a lot of {mycolumn}!"
+myheading1 = f"Wow! That's a lot of {mycolumn}!"
 mygraphtitle = '2011 US Agriculture Exports by State'
 mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
 mycolorbartitle = "Millions USD"
